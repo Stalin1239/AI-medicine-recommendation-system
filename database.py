2046c0f5d4b83b6410211b489aa732516f6cc8c2
stalin dsilva <stalindsilva26@gmail.com>
2026-06-02 12:11:43 +0530

    fix: Add missing Float import in database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sys
 import pandas as pd
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey, Boolean, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
